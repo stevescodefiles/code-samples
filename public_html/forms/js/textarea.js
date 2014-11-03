964b653495f45cce32d54bdd021c6f714c893828
inspiredsteve <sjguinns@gmail.com>
2014-11-03 22:00:45 +0000

    textarea counter and maxlength example

--- public_html/forms/js/textarea.js
+++ public_html/forms/js/textarea.js
@@ -1,22 +1,11 @@
 /* 
- * Created by Steve Guinn 
- * Angluar and select lists example
+ * Angluar and textareas
  */
 
 angular.module('notesApp', [])
     .controller('MainCtrl', [function(){
         var self = this;
-        
-        self.countries = [
-            {label: 'France', id: 1},
-            {label: 'Greece', id: 2},
-            {label: 'Denmark', id: 3},
-            {label: 'England', id: 4}
-        ];
-        
-        this.selectedCountryId = 4;
-        this.selectedCountry = self.countries[0];
-        
+    
 }]);
 
 
